0ed2da5a9a4eb982333cee89ec88dedf0ffd28b6
Георгий Хугаев <hugaeag@gmail.com>
2023-12-22 21:40:17 +0300

    remove files

--- db/migration/000001_init_schema.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE Table Otp(
-    Id SERIAL PRIMARY KEY,
-    Recipient VARCHAR(50),
-    Thema_message VARCHAR(255),
-    Body_message VARCHAR(255),
-    Date_of_sending DATE
-)
--- internal/model/otp/data.go
+++
@@ -1,9 +0,0 @@
-package otp
-
-type OtpBody struct {
-	Id            uint
-	Recipient     string
-	BodyMessage   string
-	ThemaMessage  string
-	DateOfSending string
-}
